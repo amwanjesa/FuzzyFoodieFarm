c33678a703ab5f43418f1ce267f32c90574f2516
amwanjesa <a.mwanjesa@gmail.com>
2016-12-04 18:23:03 +0100

    cleaned up data: run to get clean numerical values for certain tags

--- cluster_food.py
+++ cluster_food.py
@@ -1,1 +1,24 @@
-import sklearn 
+import click
+import numpy as np
+import pandas as pd
+
+@click.command()
+@click.argument('data_filename')
+def cluster(data_filename):
+	df =  pd.read_csv(data_filename, dtype=str)
+	data = []
+	names = []
+	for name in df.columns.values:
+		if containsNumbers(df[name]):
+			data.append(df[name])
+			names.append(name)
+	np.save('food_facts', np.array(data))
+	np.save('fact_tags', np.array(names))
+
+def containsNumbers(column):
+	for i in column:
+		if isinstance(i, float) or isinstance(i, int):
+			return True
+	return False
+if __name__ == '__main__':
+	cluster()
