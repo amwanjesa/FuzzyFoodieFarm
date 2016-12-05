1c1c11e4391e02bfd997bd12ef3310835bdb134f
amwanjesa <a.mwanjesa@gmail.com>
2016-12-05 14:41:25 +0100

    fixed index in data array bug

--- cluster_food.py
+++ cluster_food.py
@@ -8,8 +8,8 @@ import skfuzzy as fuzz
 @click.argument('data-filename')
 def cluster(data_filename):
 	data = np.load(data_filename)
+	data = data[:,1:]
 	clf = 	fuzz.cmeans(data, c=6, m=0.01, error=0.0001, maxiter=2000)
-	import pdb; pdb:set_trace()
 	print clf
 
 if __name__ == '__main__':
