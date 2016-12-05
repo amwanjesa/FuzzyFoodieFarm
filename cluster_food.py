1fa38722b51274213a8301c3157b66c5b6c47de8
amwanjesa <a.mwanjesa@gmail.com>
2016-12-05 14:27:52 +0100

    initial cluster run

--- cluster_food.py
+++ cluster_food.py
@@ -2,14 +2,15 @@ import click
 import numpy as np
 import pandas as pd
 import pdb
+import skfuzzy as fuzz
 
 @click.command()
 @click.argument('data-filename')
-@click.argument('output-filename')
-def cluster(data_filename, output_filename):
-	df =  pd.read_csv(data_filename, index_col=0, encoding='utf-8')
-	df = df._get_numeric_data()
-	df.to_csv(output_filename)
+def cluster(data_filename):
+	data = np.load(data_filename)
+	clf = 	fuzz.cmeans(data, c=6, m=0.01, error=0.0001, maxiter=2000)
+	import pdb; pdb:set_trace()
+	print clf
 
 if __name__ == '__main__':
 	cluster()
