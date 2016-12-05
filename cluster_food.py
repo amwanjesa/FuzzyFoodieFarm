c2e108ae331e6a20357034404f521ba39286eac6
amwanjesa <a.mwanjesa@gmail.com>
2016-12-05 13:04:33 +0100

    exctracting numeric data fails

--- cluster_food.py
+++ cluster_food.py
@@ -6,19 +6,27 @@ import pandas as pd
 @click.argument('data_filename')
 def cluster(data_filename):
 	df =  pd.read_csv(data_filename, dtype=str)
-	data = []
-	names = []
-	for name in df.columns.values:
-		if containsNumbers(df[name]):
-			data.append(df[name])
-			names.append(name)
-	np.save('food_facts', np.array(data))
-	np.save('fact_tags', np.array(names))
+	numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+	newdf = df.select_dtypes(include=numerics)
+	import pdb; pdb.set_trace()
+	print df
+	# data = []
+	# names = []
+	# for name in df.columns.values:
+	# 	if containsNumbers(df[name]):
+	# 		print df[name]
+	# 		data.append(df[name])
+	# 		names.append(name)
+	# data = np.array(data)
+	# names = np.array(names)
+	# import pdb; pdb.set_trace()
+	# np.save('food_facts', data)
+	# np.save('fact_tags', names)
 
 def containsNumbers(column):
 	for i in column:
-		if isinstance(i, float) or isinstance(i, int):
-			return True
+		if (isinstance(i, float) or isinstance(i, int)) and (not isinstance(i, str)):
+			return True 
 	return False
 if __name__ == '__main__':
 	cluster()
