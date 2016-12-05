2112dc71e933f1c7aedceef62b73f85bd4dd826a
amwanjesa <a.mwanjesa@gmail.com>
2016-12-05 14:02:11 +0100

    data preprocessing ready

--- cluster_food.py
+++ cluster_food.py
@@ -1,32 +1,15 @@
 import click
 import numpy as np
 import pandas as pd
+import pdb
 
 @click.command()
-@click.argument('data_filename')
-def cluster(data_filename):
-	df =  pd.read_csv(data_filename, dtype=str)
-	numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-	newdf = df.select_dtypes(include=numerics)
-	import pdb; pdb.set_trace()
-	print df
-	# data = []
-	# names = []
-	# for name in df.columns.values:
-	# 	if containsNumbers(df[name]):
-	# 		print df[name]
-	# 		data.append(df[name])
-	# 		names.append(name)
-	# data = np.array(data)
-	# names = np.array(names)
-	# import pdb; pdb.set_trace()
-	# np.save('food_facts', data)
-	# np.save('fact_tags', names)
+@click.argument('data-filename')
+@click.argument('output-filename')
+def cluster(data_filename, output_filename):
+	df =  pd.read_csv(data_filename, index_col=0, encoding='utf-8')
+	df = df._get_numeric_data()
+	df.to_csv(output_filename)
 
-def containsNumbers(column):
-	for i in column:
-		if (isinstance(i, float) or isinstance(i, int)) and (not isinstance(i, str)):
-			return True 
-	return False
 if __name__ == '__main__':
 	cluster()
