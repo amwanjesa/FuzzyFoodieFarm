b9a9c04bb74fa4d539778966d6f8a2e76910f3d2
amwanjesa <a.mwanjesa@gmail.com>
2016-12-11 15:21:23 +0100

    Clustering and classifying ready for data

--- cluster_food.py
+++ cluster_food.py
@@ -6,11 +6,14 @@ import skfuzzy as fuzz
 
 @click.command()
 @click.argument('data-filename')
-def cluster(data_filename):
+def cluster_n_predict(data_filename):
 	data = np.load(data_filename)
 	data = data[:,1:]
 	clf = 	fuzz.cmeans(data, c=6, m=0.01, error=0.0001, maxiter=2000)
-	print clf
+	classified = []
+	for ingredients in recipe:
+		prediction = fuzz.cmeans_predict(ingredients, clf.cntr)
+		classified.append(prediction)
 
 if __name__ == '__main__':
 	cluster()
