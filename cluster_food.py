fa99c29d9f17595440b205373a9fdd74af1d9eab
amwanjesa <a.mwanjesa@gmail.com>
2016-12-12 14:54:02 +0100

    FINAL FINAL filtering nutrients commit

--- cluster_food.py
+++ cluster_food.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pdb
 import skfuzzy as fuzz
 from difflib import SequenceMatcher
+from nutrition import nutritions
 
 @click.command()
 @click.argument('data-filename')
@@ -13,14 +14,21 @@ def cluster(data_filename, labels_filename):
 	labels = np.load(labels_filename)
 	data = data[:,1:]
 	clf = 	fuzz.cmeans(data, c=6, m=10, error=0.0001, maxiter=2000)
-	filter_by_nutrients(list(labels))
+	recipe_labels = nutritions()
+	nutrient_vector = filter_by_nutrients(list(labels), recipe_labels)
 
-def filter_by_nutrients(labels):
-	test_labels = [u'Iron', u'Sodium', u'Fluoride', u'Selenium', u'Trans Fat', u'Phosphorus', u'Vitamin B2', u'Caffeine', u'Vitamin A', u'Vitamin B1', u'Mono Unsaturated Fat', u'Fiber', u'Vitamin K', u'Vitamin C', u'Sugar', u'Alcohol', u'Vitamin B3', u'Fat', u'Vitamin B6', u'Potassium', u'Cholesterol', u'Folate', u'Carbohydrates', u'Copper', u'Vitamin E', u'Vitamin B5', u'Saturated Fat', u'Manganese', u'Poly Unsaturated Fat', u'Calories', u'Zinc', u'Vitamin B12', u'Protein', u'Vitamin D', u'Magnesium', u'Calcium']
+def filter_by_nutrients(labels, recipe):
+	recipe_labels = recipe.keys()
 	filtered = []
 	for label in labels:
-		filtered.append(checkSimilarity(label, test_labels, filtered))
-	print filtered
+		filtered.append(checkSimilarity(label, recipe_labels, filtered))
+	nutrient_vector = []
+	for index in filtered:
+		if isinstance(index, str) or isinstance(index, unicode):
+			nutrient_vector.append(recipe[index])
+		else:
+			nutrient_vector.append(0)
+	return nutrient_vector
 
 def checkSimilarity(nutrient, labels, filtered):
 	for nutrition in labels:
--- nutrition.py
+++ nutrition.py
@@ -1,10 +1,10 @@
 import click
 import pdb
 import unirest
-import pandas as pd 
+import pandas as pd
 
 
-def nutritions(): 
+def nutritions():
 	response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/541691/information?includeNutrition=true",
 	  headers={
 	    "X-Mashape-Key": "uI9T1GTt8nmshcUUWJOjq8TQNGBgp1P9Zffjsn7dAbkmTSDt1k",
@@ -16,7 +16,4 @@ def nutritions():
 	selectedNutritions = ['Calories', 'Fat', 'Saturated Fat', 'Sugar']
 	recipeNutritions = recipeInfo['nutrition']['nutrients']
 	nutritionsDict = {nutrition['title']: nutrition['amount'] for nutrition in recipeNutritions if nutrition['title'] in selectedNutritions}
-	print nutritionsDict
-
-if __name__ == '__main__':
-	nutritions(); 
+	return nutritionsDict
