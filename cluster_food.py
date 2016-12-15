2e0e5151e984840a56fa7cdfd7d96f23106045bf
ukhuranauva <ukhuranauva@users.noreply.github.com>
2016-12-15 22:32:49 +0100

    larger testset

--- cluster_food.py
+++ cluster_food.py
@@ -18,9 +18,13 @@ def cluster(data_filename, labels_filename):
 	labels = np.load(labels_filename)
 	data = data[:,1:]
 	clf = 	fuzz.cmeans(data, c=6, m=10, error=0.0001, maxiter=2000)
-	recipe_labels = nutritions()
-	nutrient_vector = filter_by_nutrients(list(labels), recipe_labels)
-	testClustering(data, np.array(nutrient_vector))
+    recipe_labels = json.load(open("recipesNutrients.txt"))
+    nutritions = []
+    for recipe in recipe_labels: 
+        nutrient_vector = filter_by_nutrients(list(labels), recipe)
+        nutritions.append(nutrient_vector)
+
+    testClustering(data, np.array(nutritions))
 
 
 def filter_by_nutrients(labels, recipe):
@@ -48,7 +52,7 @@ def checkSimilarity(nutrient, labels, filtered):
 
 def testClustering(train_data, test_data):
 	m = 1.1
-	ncenters = 8
+	ncenters = 6
 
 	cntr_train, u_train, u0, d, jm, p, fpc = fuzz.cluster.cmeans(train_data, ncenters, m, error=0.005, maxiter=1000, init=None)
 
