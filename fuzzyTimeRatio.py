4522427a17c6619dc99c29f492746f32af9b02bc
amwanjesa <a.mwanjesa@gmail.com>
2016-12-19 23:11:06 +0100

    module to check what kind of time the user has

---
+++ fuzzyTimeRatio.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+import matplotlib
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt
+
+
+
+def member_of_function(crisp_value):
+    # antecedent
+    ratio = ctrl.Antecedent(np.arange(0, 2.1, 0.1), 'ratio')
+
+    abcd = [[0, 0, 0.25, 0.5],
+            [0.25, 0.5, 0.75, 1.25],
+            [0.75, 1.25, 1.5, 1.75],
+            [1.5, 1.75, 2, 2]]
+
+    # Custom membership functions can be built interactively with a familiar,
+    # Pythonic API
+    ratio['ample time'] = fuzz.trapmf(ratio.universe, abcd[0])
+    ratio['comfortable'] = fuzz.trapmf(ratio.universe, abcd[1])
+    ratio['hasty'] = fuzz.trapmf(ratio.universe, abcd[2])
+    ratio['impossible'] = fuzz.trapmf(ratio.universe, abcd[3])
+
+    degrees = []
+    keys = ratio.terms.keys()
+
+    # computing membership degree for all linguistic variables
+    for i, label in enumerate(keys):
+        degrees.append(compute_degree(ratio, crisp_value, abcd[i]))
+
+    #returning linguistic variable for highest membership degree
+    return keys[degrees.index(max(degrees))]
+
+
+def compute_degree(ratio, crisp_value, abcd):
+    return fuzz.interp_membership(ratio.universe, fuzz.trapmf(ratio.universe, abcd), crisp_value)
+
+if __name__ == '__main__':
+    print member_of_function(1.4)
