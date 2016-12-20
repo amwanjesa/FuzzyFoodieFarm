18fd1e017b7eecd2d770d56c741750ac6756076b
ukhuranauva <ukhuranauva@users.noreply.github.com>
2016-12-20 13:53:44 +0100

    added casting to 2 for higher ratios

--- fuzzyTimeRatio.py
+++ fuzzyTimeRatio.py
@@ -1,13 +1,8 @@
-
-# coding: utf-8
-
 import matplotlib
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
-
-
 
 def member_of_function(crisp_value):
     # antecedent
@@ -23,8 +18,8 @@ def member_of_function(crisp_value):
     ratio['ample time'] = fuzz.trapmf(ratio.universe, abcd[0])
     ratio['comfortable'] = fuzz.trapmf(ratio.universe, abcd[1])
     ratio['hasty'] = fuzz.trapmf(ratio.universe, abcd[2])
-    ratio['impossible'] = fuzz.trapmf(ratio.universe, abcd[3])
-
+    ratio['impossible'] = fuzz.trapmf(ratio.universe, abcd[3])   
+    
     degrees = []
     keys = ratio.terms.keys()
 
@@ -37,7 +32,10 @@ def member_of_function(crisp_value):
 
 
 def compute_degree(ratio, crisp_value, abcd):
+    # cast a higher ratio to 2 since that will always be impossible to make 
+    if crisp_value > 2: 
+        crisp_value = 2
     return fuzz.interp_membership(ratio.universe, fuzz.trapmf(ratio.universe, abcd), crisp_value)
 
 if __name__ == '__main__':
-    print member_of_function(1.4)
+    print member_of_function(0.1)
